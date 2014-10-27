205943c2b6429b7226944309bab399be2dfa0e8d
Gery Debongnie <gery.debongnie@gmail.com>
2014-10-27 09:29:44 +0100

    add class support to moebius.js

--- src/moebius.js
+++ src/moebius.js
@@ -1,4 +1,21 @@
 
+function createClass () {
+    var props = (arguments[1] ? arguments[1] : arguments[0]),
+        prop_names = Object.keys(props),
+        Parent = arguments[1] && arguments[0];
+
+    var Constructor = props.init || function () {
+        Parent && Parent.apply(this, arguments);
+    };
+    if (Parent) Constructor.prototype = Object.create(Parent.prototype);
+    for (var i = 0; i < prop_names.length; i++) {
+        if (prop_names[i] !== 'init')
+            Constructor.prototype[prop_names[i]] = props[prop_names[i]];
+    }
+    return Constructor;
+}
+module.exports.createClass = createClass;
+//-----------------------------------------------------------------------------
 var handlers = {};
 
 function createAction (name) {
@@ -21,89 +38,89 @@ function createActions () {
 module.exports.createActions = createActions;
 
 //-----------------------------------------------------------------------------
-function VNode () {}
-VNode.prototype.appendTo = function () {};
-VNode.prototype.destroy = function () {};
+var VNode = createClass({
+    appendTo: function () {},
+    destroy: function () {},
+});
 
 //-----------------------------------------------------------------------------
-function HtmlVNode (tagName, props) {
-    this.node = document.createElement(tagName);
-    this.children = [];
-    var children = Array.prototype.slice.call(arguments, 2);
-    for (var j = 0; j < children.length; j++) {
-        this.children = this.children.concat(children[j]);
-    }
-    var keys = Object.keys(props || {});
-    for (var i = 0; i < keys.length; i++) {
-        if (keys[i] === 'className') {
-            if (props.className instanceof Array) {
-                this.node.classList.add.apply(this.node.classList, props.className);
-            } else
-                this.node.classList.add(props.className);            
-        } else if (keys[i] === 'onClick') {
-            this.node.addEventListener('click', props.onClick);
-        } else {
-            this.node.setAttribute(keys[i], props[keys[i]]);
+var HtmlVNode = createClass(VNode, {
+    init: function (tagName, props) {
+        this.node = document.createElement(tagName);
+        this.children = [];
+        var children = Array.prototype.slice.call(arguments, 2);
+        for (var j = 0; j < children.length; j++) {
+            this.children = this.children.concat(children[j]);
         }
-    }
-}
-HtmlVNode.prototype = Object.create(VNode.prototype);
-
-HtmlVNode.prototype.appendTo = function (node) {
-    this.parentNode = node;
-    for (var i = 0; i < this.children.length; i++) {
-        if (typeof this.children[i] === 'string') {
-            this.node.appendChild(document.createTextNode(this.children[i]));
+        var keys = Object.keys(props || {});
+        for (var i = 0; i < keys.length; i++) {
+            if (keys[i] === 'className') {
+                if (props.className instanceof Array) {
+                    this.node.classList.add.apply(this.node.classList, props.className);
+                } else
+                    this.node.classList.add(props.className);            
+            } else if (keys[i] === 'onClick') {
+                this.node.addEventListener('click', props.onClick);
+            } else {
+                this.node.setAttribute(keys[i], props[keys[i]]);
+            }
         }
-        if (this.children[i] instanceof VNode) {
-            this.children[i].appendTo(this.node);
+    },
+    appendTo: function (parentNode) {
+        this.parentNode = parentNode;
+        for (var i = 0; i < this.children.length; i++) {
+            if (typeof this.children[i] === 'string') {
+                this.node.appendChild(document.createTextNode(this.children[i]));
+            }
+            if (this.children[i] instanceof VNode) {
+                this.children[i].appendTo(this.node);
+            }
         }
-    }
-    node.appendChild(this.node);
-};
-HtmlVNode.prototype.destroy = function () {
-    for (var i = 0; i < this.children.length; i++) {
-        if (this.children[i] instanceof VNode) {
-            this.children[i].destroy();
+        parentNode.appendChild(this.node);        
+    },
+    style: function (css) {
+        var props = Object.keys(css);
+        for (var i = 0; i < props.length; i++) {
+            this.node.style[props[i]] = css[props[i]];
         }
-    }
-    this.parentNode.removeChild(this.node);
-    this.node = undefined;
-    this.parentNode = undefined;
-};
-HtmlVNode.prototype.style = function (css) {
-    var props = Object.keys(css);
-    for (var i = 0; i < props.length; i++) {
-        this.node.style[props[i]] = css[props[i]];
-    }
-    return this;
-};
+        return this;
+    },
+    destroy: function () {
+        for (var i = 0; i < this.children.length; i++) {
+            if (this.children[i] instanceof VNode) {
+                this.children[i].destroy();
+            }
+        }
+        this.parentNode.removeChild(this.node);
+        this.node = undefined;
+        this.parentNode = undefined;
+    },
+});
 
 //-----------------------------------------------------------------------------
-function Component (props) {
-    this.props = props || {};
-    this.children = Array.prototype.slice.call(arguments, 1);
-}
-Component.prototype = Object.create(VNode.prototype);
-
-Component.prototype.render = function () {};
-Component.prototype.appendTo = function (parentNode) {
-    this.parentNode = parentNode;
-    this.vnode = this.render();
-    if (this.vnode) this.vnode.appendTo(parentNode);
-};
-Component.prototype.destroy = function () {
-    if (this.parentNode && this.vnode) {
-        this.vnode.destroy();
-        this.vnode = null;
-        this.parentNode = null;
-    }
-};
-Component.prototype.onAction = function (name, handler) {
-    if (name in handlers) handlers[name].push(handler);
-    else handlers[name] = [handler];
-};
-
+var Component = createClass(VNode, {
+    init: function (props) {
+        this.props = props || {};
+        this.children = Array.prototype.slice.call(arguments, 1);
+    },
+    render: function () {},
+    appendTo: function (parentNode) {
+        this.parentNode = parentNode;
+        this.vnode = this.render();
+        if (this.vnode) this.vnode.appendTo(parentNode);
+    },
+    onAction: function (name, handler) {
+        if (name in handlers) handlers[name].push(handler);
+        else handlers[name] = [handler];
+    },
+    destroy: function () {
+        if (this.parentNode && this.vnode) {
+            this.vnode.destroy();
+            this.vnode = null;
+            this.parentNode = null;
+        }
+    },
+});
 module.exports.Component = Component;
 
 //-----------------------------------------------------------------------------
